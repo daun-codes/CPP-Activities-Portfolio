d1c0286ab874244cbe5cd3f658576263dc7d2fc9
donanalyst <jervickamodia@gmail.com>
2026-02-24 00:19:13 +0800

    Added a new Project Library Management System

---
+++ Activity-8-Library Management System/book.cpp
@@ -0,0 +1,33 @@
+#include "book.h"
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+
+// Constructor for Backup File 
+Book::Book(std::string bookID, std::string bookTitle, std::string author, std::string ISBN, std::string category, std::string publisher, std::string yearPublished, std::string status, int totalCopies, int availCopies, int borrowedCopies)
+    : bookID(bookID),
+    bookTitle(bookTitle),
+    author(author),
+    ISBN(ISBN),
+    category(category),
+    publisher(publisher),
+    yearPublished(yearPublished),
+    status(status),
+    totalCopies(totalCopies),
+    availCopies(availCopies),
+    borrowedCopies(borrowedCopies)
+{
+}
+
+
+// Initial Constructor for new Book that has no Status yet
+Book::Book(std::string bookID, std::string bookTitle, std::string author,
+    std::string ISBN, std::string category, std::string publisher,
+    std::string yearPublished, std::string status,
+    int totalCopies, int availCopies)
+    : Book(bookID, bookTitle, author, ISBN, category, publisher,
+        yearPublished, status, totalCopies, availCopies, 0) // ✅ borrowedCopies = 0
+{
+}
+
